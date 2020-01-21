7ce57cb3cd9f2ee44f3af8254fe8c6372ad0f1c3
kvimalkr55 <kvimalkr55@gmail.com>
2020-01-21 22:08:27 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    AboutComponent,
+    ProductComponent,
+    ContactComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
