2bbbda24ba5ed106fd6351419d796828ab2e7fcb
kvimalkr55 <kvimalkr55@gmail.com>
2020-01-25 17:27:37 +0530

    all

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
+  selector: '.app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+ name;
+ usersDetails = [
+   {
+     name: 'one',
+     image: './../../assets/images.jpg',
+     description: 'Some example text some example text. John Doe is an architect and engineer'
+   },
+   {
+    name: 'two',
+    image: './../../assets/images.jpg',
+    description: 'Some example text some example text. John Doe is an architect and engineer'
+  },
+  {
+    name: 'three',
+    image: './../../assets/images.jpg',
+    description: 'Some example text some example text. John Doe is an architect and engineer'
+  },
+  {
+    name: 'four',
+    image: './../../assets/images.jpg',
+    description: 'Some example text some example text. John Doe is an architect and engineer'
+  }
+ ]
+  constructor() {
+    this.name = 'on'; 
+   }
 
   ngOnInit() {
+  }
+
+  getServer(){
+    if(this.name == 'on'){
+      return this.name = 'off';
+    }
+      return this.name = 'on';
   }
 
 }
