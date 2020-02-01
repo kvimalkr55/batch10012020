13b873029ce018267a20178a771699b47c5c38f2
kvimalkr55 <kvimalkr55@gmail.com>
2020-02-01 10:49:00 +0530

    ngclass, ngstyle

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,21 +9,28 @@ export class AboutComponent implements OnInit {
  name;
  usersDetails = [
    {
+     color:'green',
+     gender: 'F',
      name: 'one',
      image: './../../assets/images.jpg',
      description: 'Some example text some example text. John Doe is an architect and engineer'
    },
    {
+    color:'pink',
+    gender: 'M',
     name: 'two',
     image: './../../assets/images.jpg',
     description: 'Some example text some example text. John Doe is an architect and engineer'
   },
   {
+    color:'yellow',
+    gender: 'F',
     name: 'three',
     image: './../../assets/images.jpg',
     description: 'Some example text some example text. John Doe is an architect and engineer'
   },
   {
+    color:'blue',
     name: 'four',
     image: './../../assets/images.jpg',
     description: 'Some example text some example text. John Doe is an architect and engineer'
